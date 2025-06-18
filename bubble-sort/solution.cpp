bfb58fdc691d0766aaa8365379a002b14b4a709d
Santhoshi Boyina <boyinasanthoshi12@gmail.com>
2025-06-18 19:34:10 +0530

    bubble sort solution

---
+++ bubble-sort/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void display(vector<int> &arr, int n) {
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+void bubbleSort(vector<int> &arr, int n) {
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        display(arr, n);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr (n);
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+    return 0;
+}
